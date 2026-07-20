0858eadc6ca403c396fa7be06ddd68dc978ca54c
beffenderMANIPULATOR <sy2ryxmnmy@privaterelay.appleid.com>
2026-07-20 20:14:21 +0200

    Clean up comments in gui.h
    Removed comments related to GUI functionality.

--- src/gui.h
+++ src/gui.h
@@ -1,11 +1,8 @@
 #pragma once
 
-// GUI des Plugins (ImGui + Win32 + DirectX 11).
+// gui etc
 namespace atomic::gui
 {
-    // Blockiert, bis das Fenster geschlossen wird. Laeuft im eigenen Thread.
     void RunGuiThread(void* hModule);
-
-    // Signalisiert dem GUI-Thread, sich zu beenden.
     void RequestShutdown();
 }
