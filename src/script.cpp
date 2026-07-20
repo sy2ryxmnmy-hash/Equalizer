ba79bcae68fcc47d88f61f184d744fddb8ece241
beffenderMANIPULATOR <sy2ryxmnmy@privaterelay.appleid.com>
2026-07-20 20:16:00 +0200

    Refactor Init and Shutdown script functions

--- src/script.cpp
+++ src/script.cpp
@@ -2,39 +2,29 @@
 
 #include <cstdio>
 
-// ============================================================================
-//  script.cpp
-//  Hier kommen alle Scripts rein, die beim Initialisieren laufen sollen.
-//  Einfach unten in RunInitScripts() weitere Aufrufe ergaenzen.
-// ============================================================================
-
 namespace atomic::script
 {
-    // ------------------------------------------------------------------
-    // Beispiel-Script: hier eigene Init-Logik einbauen
-    // ------------------------------------------------------------------
     static void InitScript_Example()
     {
-        // TODO: eigenes Init-Script hier
+        // NOTE: Init-Script bzw bd
     }
 
     void RunInitScripts()
     {
         InitScript_Example();
-        // Weitere Init-Scripts hier aufrufen:
-        // InitScript_MeinScript();
+        // hier initializen
     }
 
     void RunShutdownScripts()
     {
-        // Aufraeum-Scripts hier aufrufen
+        // shutdown callen
     }
 
     bool StartLoudmice(char* outError, int outErrorSize)
     {
         // Noch nicht implementiert -> Fehler zurueckgeben
         std::snprintf(outError, outErrorSize,
-                      "Error: 'Start loudmice' ist noch nicht implementiert.");
+                      "Error: 'Start loudmice' ist noch nicht implemented.");
         return false;
     }
 }
