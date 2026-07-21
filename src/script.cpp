41e53ce0ce04c3daaef863274441f126e14d2bb9
Steuerhinterzieher <sy2ryxmnmy@privaterelay.appleid.com>
2026-07-21 22:47:52 +0200

    Update script.cpp

--- src/script.cpp
+++ src/script.cpp
@@ -6,23 +6,23 @@ namespace atomic::script
 {
     static void InitScript_Example()
     {
-        // NOTE: Init-Script bzw bd
+        // NOTE: here Initialize Script
     }
 
     void RunInitScripts()
     {
         InitScript_Example();
-        // hier initializen
+        // the initialize
     }
 
     void RunShutdownScripts()
     {
-        // shutdown callen
+        // calling the shutdown
     }
 
     bool StartLoudmice(char* outError, int outErrorSize)
     {
-        // Noch nicht implementiert -> Fehler zurueckgeben
+        // Fake error
         std::snprintf(outError, outErrorSize,
                       "Error: 'Start loudmice' ist noch nicht implemented.");
         return false;
